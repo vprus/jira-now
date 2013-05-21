9eae6749ce78432b0e54d670dcafac08b5bc20a2
Vladimir Prus <vladimir@codesourcery.com>
2013-05-21 20:49:34 +0400

    Make cookie expire after 6 months, not 10 days.

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,8 @@ exports.partials = function (req, res) {
 };
 
 exports.setup = function (req, res) {
-  res.cookie('clientConfig', req.params.clientConfig, { maxAge: 900000});
+  var d = new Date();
+  d.setTime(d.getTime() + 6*30*24*60*60*1000);
+  res.cookie('clientConfig', req.params.clientConfig, { expires: d});
   res.redirect("/");
 }
